cc46379ce98d36deb64a7ea10ddaadb42b6798f8
Aman Kumar <amankarguwal0@gmail.com>
2024-04-23 18:21:37 +0530

    commit 2
    created number counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,16 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  let counter = 15;
+  let [counter , setCounter] = useState(15)
+
+  // let counter = 15;
 
   const addValue = () => {
     // console.log("value added" , Math.random())
-    counter = counter + 1;
+    // counter = counter + 1;
+    if(counter < 20){
+      setCounter(counter + 1);
+    }
     // console.log("clicked" , Math.random())
-    console.log("clicked" , Math.random())
+    // console.log("clicked" , counter)
 
+  }
+
+  const subtractValue = () => {
+    if(counter > 0){
+      // counter = counter - 1;
+      setCounter(counter - 1); 
+    }
   }
 
 
@@ -21,13 +32,12 @@ function App() {
   return (
     <>
       <h1>Chai aur react</h1>
-      <h2>Counter Value: {counter}</h2>
-      <button
-        onClick={addValue}
-      >Add value</button>
+      <h2>Counter Value: {counter} </h2>
+      <button onClick={addValue}>Add value {counter} </button>
       <br/>
       <br/>
-      <button>Subtract value</button>
+      <button onClick={subtractValue}>Subtract value {counter} </button>
+      <p>footer : {counter} </p>
     </>
   )
 }
