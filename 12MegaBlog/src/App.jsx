5a6bbf75771ad93ad70d681f907fde4024737323
Aman Kumar <amankarguwal0@gmail.com>
2024-04-26 19:18:11 +0530

    MegaBlog commit 4
    Auth of Login And signup and Store created

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,16 +1,42 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login , logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+import { Header , Footer } from './components'
+
 
 function App() {
-  // console.log(process.env.REACT_APP_APPWRITE_URL) // for react appwrite url
 
-  console.log(import.meta.env.VITE_APPWRITE_URL) // for vite appwrite url
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>
-        A blog app with AppWrite
-      </h1>
-    </>
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  } , [])
+
+  return ( !loading ? (
+    <div className=' text-center min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+        TODO : <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+    ) : (
+    <div>Loading...</div>
+    )
   )
 }
 
