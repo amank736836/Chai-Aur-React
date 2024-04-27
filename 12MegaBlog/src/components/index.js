32313c9d20e2b2e81e9c3e47f03b7ceef6c94faa
Aman Kumar <amankarguwal0@gmail.com>
2024-04-27 14:41:22 +0530

    MegaBlog - commit 7
    PostForm and Editor component created

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -5,4 +5,5 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
-export { Header, Footer , Container , Logo , LogoutBtn , Input , Button};
+import RTE from "./RTE";
+export { Header, Footer , Container , Logo , LogoutBtn , Input , Button , RTE};
