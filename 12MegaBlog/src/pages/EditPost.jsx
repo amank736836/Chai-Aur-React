39e62574b559221c62e8693b6dbfb295cb6e05a2
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:55:27 +0530

    refactor: Update EditPost component to improve code readability and maintainability
    Refactor the EditPost component to improve code readability and maintainability. Changes include:
    - Reformatting code to adhere to consistent coding style
    - Using double quotes consistently for string literals
    - Updating classNames to use tailwind CSS utility classes
    - Adding lazy loading for images to improve page load performance
    - Rearranging CSS classes for improved layout and responsiveness
    - Updating component imports to use file extensions for consistency

--- 12MegaBlog/src/pages/EditPost.jsx
+++ 12MegaBlog/src/pages/EditPost.jsx
@@ -1,35 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Container, PostForm } from '../components'
-import appwriteService from '../appwrite/config'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Container, PostForm } from "../components";
+import appwriteService from "../appwrite/config";
 
+function EditPost () {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
 
-function EditPost() {
-    const [post, setPost] = useState(null)
-    const {slug} = useParams()
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        if(slug){
-            appwriteService.getPost(slug).then((post) => {
-                if (post) {
-                    setPost(post)
-                }
-            })
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
         }
-        else{
-            navigate('/')
-        }
-    }, [slug, navigate])
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
 
-  return ( post ? (
-    <div className='py-8'>
-        <Container>
-            <PostForm post={post} />
-        </Container>
+  return post ? (
+    <div className="py-8">
+      <Container>
+        <PostForm post={post} />
+      </Container>
     </div>
-  ) : null
-  )
-}
+  ) : null;
+};
 
-export default EditPost
+export default EditPost;
