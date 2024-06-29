9476ab244a4eca71bde73b06fcdb5a6b24eafb29
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:52:36 +0530

    refactor: Update Header component to improve code readability and maintainability
    The Header component has been refactored to improve code readability and maintainability. Changes include:
    - Reformatting code to adhere to consistent coding style
    - Using double quotes consistently for string literals
    - Updating classNames to use tailwind CSS utility classes
    - Adding support for dark mode by updating the background color and hover color
    - Adding a ThemeBtn component for toggling theme mode
    - Adding lazy loading for images to improve page load performance

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,74 +1,76 @@
-import React from 'react'
-import {Container , Logo , LogoutBtn} from '../index'
-import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Container, Logo, LogoutBtn, ThemeBtn } from "../index.js";
 
 function Header() {
-  const authStatus = useSelector((state) => state.auth.status)
-  const navigate = useNavigate()
+  const authStatus = useSelector((state) => state.status);
+  const navigate = useNavigate();
 
   const navItems = [
     {
-      name:'Home',
-      slug:'/',
-      active: true
+      name: "Home",
+      slug: "/",
+      active: true,
     },
     {
-      name:'login',
-      slug:'/login',
-      active: !authStatus
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
     },
     {
-      name:'Signup',
-      slug:'/signup',
-      active: !authStatus
-    },{
-      name:'All Posts',
-      slug:'/all-posts',
-      active: authStatus
-    },{
-      name:'Add Post',
-      slug:'/add-post',
-      active: authStatus
-    }
-  ]
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
 
   return (
-    <Header className="py-3 shadow bg-gray-500">
+    <header className="py-2 bg-gray-100 shadow dark:bg-gray-900 dark:text-white">
       <Container>
-        <nav className='flex'>
-          <div className='mr-4'>
-            <Link to='/'>
-              <Logo width='70px'/>
+        <nav className="flex items-center justify-between">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
             </Link>
           </div>
-          <ul className='flex ml-auto'>
-            {navItems.map((item) => {
-              return item.active ? (
-                <li key={item.name}
-                // className='mr-4'
-                >
+          <ul className="flex items-center">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name} className="ml-4">
                   <button
-                  onClick={() => navigate(item.slug)}
-                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    onClick={() => navigate(item.slug)}
+                    className="px-4 py-2 duration-200 rounded-lg hover:bg-blue-700 hover:text-white dark:hover:bg-green-700 dark:hover:text-white"
                   >
                     {item.name}
                   </button>
                 </li>
               ) : null
-              }
             )}
             {authStatus && (
-              <li>
-                <LogoutBtn/>
+              <li className="ml-4">
+                <LogoutBtn />
               </li>
             )}
+            <li className="ml-4">
+              <ThemeBtn />
+            </li>
           </ul>
         </nav>
       </Container>
-    </Header>
-  )
+    </header>
+  );
 }
 
-export default Header
+export default Header;
