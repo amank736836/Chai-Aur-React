7cc145c3286c3a40d73b8a64f95b351a1fba4389
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:53:49 +0530

    refactor: Update component imports to use file extensions
    The component imports in the index.js file have been updated to use file extensions. This ensures consistency and improves code readability. The changes include:
    - Updating imports to use file extensions (e.g., Container.jsx, Footer.jsx)
    - Rearranging the order of imports for better organization
    - Removing unnecessary imports (e.g., InitialLoader, SkeletonLoader)

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,32 +1,43 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Container from "./container/Container";
-import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Login from "./Login";
-import Button from "./Button";
+import Container from "./container/Container.jsx";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import LogoutBtn from "./header/LogoutBtn";
+import InitialLoader from "./loaders/InitialLoader";
+import Loader from "./loaders/Loader";
+import SkeletonLoader from "./loaders/SkeletonLoader";
 import PostForm from "./post-form.jsx/PostForm";
-import PostCard from "./PostCard";
+import AuthCallBack from "./AuthCallBack";
 import AuthLayout from "./AuthLayout";
+import Button from "./Button";
+import EmailVerification from "./EmailVerification";
 import Input from "./Input";
-
+import Login from "./Login";
+import Logo from "./Logo";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import ThemeBtn from "./ThemeBtn";
 
 export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn,
-    RTE,
-    Signup,
-    Login,
-    Button,
-    PostForm,
-    PostCard,
-    AuthLayout,
-    Input,
-    Select,
+  Container,
+  Footer,
+  Header,
+  LogoutBtn,
+  InitialLoader,
+  Loader,
+  SkeletonLoader,
+  PostForm,
+  AuthCallBack,
+  AuthLayout,
+  Button,
+  EmailVerification,
+  Input,
+  Login,
+  Logo,
+  PostCard,
+  RTE,
+  Select,
+  Signup,
+  ThemeBtn,
 };
