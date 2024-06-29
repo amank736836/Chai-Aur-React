72d475dfc22b9aeeb1e8d2bf0d5af1b91874f1f7
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:57:01 +0530

    refactor: Update conf.js to include TinyMCE API key
    Update the conf.js file to include the TinyMCE API key. This change ensures that the API key is properly configured and accessible within the application. By including the API key in the conf.js file, we can easily reference it in other parts of the codebase, improving code readability and maintainability.

--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,9 +1,13 @@
 const conf = {
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  tinyMCEApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+  // appwriteRedirectURL : String(import.meta.env.VITE_APPWRITE_REDIRECT_URL),
+  // successLoginURL : String(import.meta.env.VITE_SUCCESS_LOGIN_URL),
+  // failureLoginURL : String(import.meta.env.VITE_FAILURE_LOGIN_URL),
+};
 
-export default conf
+export default conf;
