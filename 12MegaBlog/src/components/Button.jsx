f2c095963cff07ca2f33fad7ffd31cc11098952b
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:43:59 +0530

    refactor: Update AuthLayout component

--- 12MegaBlog/src/components/Button.jsx
+++ 12MegaBlog/src/components/Button.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function Button({
-    children,
-    type = 'button',
-    bgColor = 'bg-blue-600',
-    textColor = 'text-white',
-    className = '',
-    ...props
+function Button ({
+  children,
+  type = "button",
+  className = "",
+  css = "bg-blue-700 dark:bg-green-700 dark:hover:bg-green-900 hover:bg-blue-600 dark:hover:text-black hover:text-white",
+  ...props
 }) {
   return (
-    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
-        {children}
+    <button
+      className={`px-4 py-2 rounded-lg ${css} ${className}`}
+      {...props}
+    >
+      {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
