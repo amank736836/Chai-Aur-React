fb8e2bd90b9e2315fc5deb116cd2dd70fbfdd584
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:56:54 +0530

    refactor: Update Home component to improve code readability and maintainability

--- 12MegaBlog/src/pages/Home.jsx
+++ 12MegaBlog/src/pages/Home.jsx
@@ -1,50 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import appwriteService from '../appwrite/config'
-import { Container, PostCard } from '../components'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button, Container } from "../components/index.js";
 
 function Home() {
-    const [posts, setPosts] = useState([])
+  const authStatus = useSelector((state) => state.status);
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-
-    if(posts.length === 0){
-        return (
-            <div className='w-full py-8 mt-4 text-center'>
-                <Container>
-                    <div className='flex flex-wrap'>
-                        <div className='p-2 w-full'>
-                            <h1 className='text-2xl font-bold hover:text-gray-500'>
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
+  return (
+    <div className="flex items-center justify-center w-full max-h-screen bg-gray-100 dark:bg-gray-900">
+      <Container>
+        <div className="flex flex-wrap justify-center pt-56 pb-56">
+          <div className="w-full text-center">
+            <h1 className="text-6xl font-bold text-center text-gray-900 dark:text-white">
+              Welcome to{" "}
+              <span className="text-blue-700 dark:text-green-900">Frame & Phrase!</span>
+            </h1>
+            <p className="mt-4 text-3xl text-center text-gray-500 dark:text-gray-300">
+              -Where prose finds its tranquil home. Every snapshot has a story, and we're here to tell it.
+            </p>
+            <div className="w-full mt-12 text-center">
+              {authStatus ? (
+                <Link to="/all-posts">
+                  <Button className="text-white transition-colors duration-300 bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900">
+                    Deep dive!
+                  </Button>
+                </Link>
+              ) : (
+                <Link to="/login">
+                  <Button className="text-white transition-colors duration-300 bg-blue-500 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-900">
+                    Get Started
+                  </Button>
+                </Link>
+              )}
             </div>
-        )
-    }
-    else{
-        return (
-            <div className='w-full py-8'>
-                <Container>
-                    <div className='flex flex-wrap'>
-                        {posts.map((post) => (
-                            <div key={post.$id} className='p-2 w-1/4'>
-                                {/* <PostCard post={post} /> */}
-                                <PostCard {...post} />
-                            </div>
-                        )
-                        )}
-                    </div>
-                </Container>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
