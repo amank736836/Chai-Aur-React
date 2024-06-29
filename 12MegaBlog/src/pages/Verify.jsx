2a954561f8ef781a3604414cf412c683bbd1a666
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:49:50 +0530

    feat: Add Verify page component
    The Verify page component has been added to handle the email verification process. It renders the EmailVerification component and provides a route for users to verify their email. This addition improves the user experience by allowing users to easily verify their email and access the application's features.

---
+++ 12MegaBlog/src/pages/Verify.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { EmailVerification } from "../components/index";
+
+function Verify () {
+  return (
+    <div className="py-8">
+      <EmailVerification />
+    </div>
+  );
+};
+
+export default Verify;
