f4c2175cad9ea9983b43d88684f9f8c2c1016ec1
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:50:51 +0530

    refactor: Update Select component to improve code readability and maintainability
    The Select component has been refactored to improve code readability and maintainability. Changes include:
    - Reformatting code to adhere to consistent coding style
    - Using double quotes consistently for string literals
    - Updating classNames to use tailwind CSS utility classes
    - Adding support for dark mode by updating the background color and hover color

--- 12MegaBlog/src/components/Select.jsx
+++ 12MegaBlog/src/components/Select.jsx
@@ -1,31 +1,26 @@
-import React, { forwardRef, useId } from 'react'
+import React, { forwardRef, useId } from "react";
 
-function Select({
-    options = [],
-    label,
-    className = '',
-    ...props
-} , ref) {
-    const id = useId();
+function Select({ options = [], label, className = "", ...props }, ref) {
+  const id = useId();
   return (
-    <div className='w-full'>
-        {label && <label htmlFor={id} className=''></label>}
-        <select
-            {...props}
-            id={id}
-            ref={ref}
-            //className passed couble by empty string rather than undefined or null
-            className={` px-3 py-2 rounded-lg bg-white text-black
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select
+        {...props}
+        id={id}
+        ref={ref}
+        //className passed couble by empty string rather than undefined or null
+        className={` px-3 py-2 rounded-lg bg-white text-black
             outline-none focus:bg-gray-50 duration-200 border border-gray-200
             w-full ${className}`}
-        >
-            {options?.map((option) => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-            ))}
-        </select>
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
-export default forwardRef(Select)
+export default forwardRef(Select);
