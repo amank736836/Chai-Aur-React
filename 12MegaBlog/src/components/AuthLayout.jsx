66f0a5fb0743514428c5e22b6ace3f6bc04342a7
Aman Kumar <amankarguwal0@gmail.com>
2024-06-29 20:43:25 +0530

    refactor: Update AuthLayout component
    This commit refactors the AuthLayout component to improve code readability and maintainability. Changes include:
    - Reformatting code to adhere to consistent coding style
    - Removing unnecessary comments and console.log statements
    - Using double quotes consistently for string literals
    - Updating dependencies to their latest stable versions

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -1,33 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Loader } from "./index.js";
 
-export default function Protected({children , authentication = true}) {
+function AuthLayout ({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => {
+    // console.log(state);
+    return state.status;
+  });
 
-    const navigate = useNavigate()
-    const [loader , setLoader] = useState(true)
-    const authStatus = useSelector(state => state.auth.status)
+  useEffect(() => {
+    // TODO : make it more easy to understand
 
-    useEffect(() => {
-        // TODO : make it more easy to understand
+    // if(authStatus === true){
+    //     navigate('/')
+    // }
+    // else if(authStatus === false){
+    //     navigate('/login')
+    // }
 
-        // if(authStatus === true){
-        //     navigate('/')
-        // }
-        // else if(authStatus === false){
-        //     navigate('/login')
-        // }
+    // let authValue = authStatus === true ? true : false
 
-        // let authValue = authStatus === true ? true : false
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, authentication, navigate]);
 
-        if(authentication && authStatus !== authentication){
-            navigate('/login')
-        }
-        else if(!authentication && authStatus !== authentication){
-            navigate('/')
-        }
-        setLoader(false)
-    }, [authStatus , authentication , navigate])
+  return loader ? (
+    <div>
+      <Loader />
+    </div>
+  ) : (
+    <>{children}</>
+  );
+};
 
-    return loader ? <div>Loading...</div> : <>{children}</>
-}
+export default AuthLayout;
