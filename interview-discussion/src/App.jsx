7b6a2ca4e0e4cbde3945065507e519e7a38bdb40
Aman Kumar <amankarguwal0@gmail.com>
2024-07-01 20:01:58 +0530

    feat: Add lazy loading for images in App component
    This commit adds lazy loading for images in the App component. By implementing lazy loading, the images will be loaded only when they are visible on the screen, improving the page load performance. This change enhances the user experience and aligns with the established code organization and setup for frontend development.

---
+++ interview-discussion/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log("App component rendered" + Math.random())
+  // const [value, setValue] = useState(1)
+  const [value, setValue] = useState({
+    value: 1,
+  })
+  // const [multipliedValue, setMultipliedValue] = useState(1)
+  // let multipliedValue = value * 5
+
+
+  // const multiplyByfive = () => {
+  //   // setMultipliedValue(value * 5)
+  //   setValue(value + 1)
+  // }
+
+  // useEffect(() => {
+  //   multipliedValue()
+  // }),[value]
+
+  const clickMe = () => {
+    console.log('logged')
+    // setValue(value * 5)
+    setValue({
+      value: 1
+    });
+  }
+
+  return (
+    <>
+      <h1> Main Value : {value.value}</h1>
+      <button
+        onClick={clickMe}
+      >Click to multiply by 5</button>
+      {/* <h1> Multiplied Value : {multipliedValue} </h1> */}
+    </>
+  )
+}
+
+export default App
